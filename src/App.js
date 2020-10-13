aaf8ce3f934b35b24a6ba54e1ac33dd2c40517fe
Dennis Idowu <dennis_idowu@live.com>
2020-10-13 14:43:53 -0400

    added state to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header/Header';
 import Figure from './components/Figure/Figure';
 import WrongLetters from './components/WrongLetters/WrongLetters';
-
+import Word from './components/Word/Word';
+import PopUp from './components/PopUp/PopUp';
 import './App.css';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+
+
+
 function App() {
+  //setting states:
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
+
     <>
       <h1>Welcome to Hangman</h1>
       <Header/>
       <div className="game-container">
          <Figure/>
          <WrongLetters/>
+         <Word/>
       </div>
     </>
   );
