02bc1a98d6d59a2262b929bf00a4a4ed4746870d
Dennis Idowu <dennis_idowu@live.com>
2020-10-13 14:31:06 -0400

    imported WrongLetters component and created Word component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Figure from './components/Figure/Figure';
+import WrongLetters from './components/WrongLetters/WrongLetters';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1>Welcome to Hangman</h1>
       <Header/>
       <div className="game-container">
          <Figure/>
+         <WrongLetters/>
       </div>
-    </div>
+    </>
   );
 }
 
