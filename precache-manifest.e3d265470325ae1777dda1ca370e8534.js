ff1332968f9ce9b48c87db93d9651c79de190b62
Dennis Idowu <dennis_idowu@live.com>
2020-11-23 13:42:22 -0500

    Updates

---
+++ precache-manifest.e3d265470325ae1777dda1ca370e8534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7dc198e3147356f3282a0d2757e7df",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "e0d003f37d1076dcecd3",
+    "url": "/hangman-react/static/css/main.40789dbf.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/hangman-react/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d003f37d1076dcecd3",
+    "url": "/hangman-react/static/js/main.b875df48.chunk.js"
+  },
+  {
+    "revision": "3f7c69ac9d5e568f51ba",
+    "url": "/hangman-react/static/js/runtime-main.0e2bf36f.js"
+  }
+]);
